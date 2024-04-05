0b8640404b9a5f2a46d65c76a34b9acf0e8d7ce9
notsreeraj <109159574+notsreeraj@users.noreply.github.com>
2024-04-05 16:53:05 -0400

    made ball class and made constructor

--- Assignment 4/part 3/main.js
+++ Assignment 4/part 3/main.js
@@ -7,16 +7,15 @@ const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
 // function to generate random number
-
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // function to generate random color
-
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
 // defining a class names ball
 class Ball{
     //created a constructor
@@ -34,4 +33,31 @@ class Ball{
         ctx.arc(this.x ,this.y ,this.size ,0 ,2*Math.PI);
         ctx.fill();
     }
+    update() {
+        if ((this.x + this.size) >= width) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.x - this.size) <= 0) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.y + this.size) >= height) {
+          this.velY = -(this.velY);
+        }
+      
+        if ((this.y - this.size) <= 0) {
+          this.velY = -(this.velY);
+        }
+      
+        this.x += this.velX;
+        this.y += this.velY;
+      }
+      
 }
+const testBall = new Ball(50,100,4,4,"blue",10);
+testBall.x;
+testBall.size;
+testBall.color;
+testBall.draw();
+
