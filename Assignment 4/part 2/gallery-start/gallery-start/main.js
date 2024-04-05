fa8964b3597cdcb7f04619d49aae6febfea68834
notsreeraj <109159574+notsreeraj@users.noreply.github.com>
2024-04-05 13:29:49 -0400

    added object literals
    declared object literals to store image alternates for each image..

--- Assignment 4/part 2/gallery-start/gallery-start/main.js
+++ Assignment 4/part 2/gallery-start/gallery-start/main.js
@@ -8,6 +8,14 @@ const overlay = document.querySelector('.overlay');
 const images = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg',];
 
 /* Declaring the alternative text for each image file */
+const alternateImages ={
+    'pic1.jpg' :  "A human eye",
+    'pic2.jpg' :  'Rock with a wavy design',
+    'pic3.jpg' :  'Purple and White Pansie Flower',
+    'pic4.jpg' :  'Sections of a wall from a Pharoahs\'s tomb',
+    'pic5.jpg' :  'Large moth on a leaf'
+
+}
 
 /* Looping through images */
 
