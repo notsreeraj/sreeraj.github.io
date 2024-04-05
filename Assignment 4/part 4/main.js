c533b374459f2e9634bc43ad2cf305ac3179f33e
notsreeraj <109159574+notsreeraj@users.noreply.github.com>
2024-04-05 18:25:35 -0400

    key controls
    added keyboard controls for the evil circle

--- Assignment 4/part 4/main.js
+++ Assignment 4/part 4/main.js
@@ -84,7 +84,26 @@ class EvilCircle extends Shape {
       this.size = 10;
     }
   
-  }
+    setControls() {
+        window.addEventListener("keydown", (e) => {
+          switch (e.key) {
+            case "a":
+              this.x -= this.velX;
+              break;
+            case "d":
+              this.x += this.velX;
+              break;
+            case "w":
+              this.y -= this.velY;
+              break;
+            case "s":
+              this.y += this.velY;
+              break;
+          }
+        });
+      }
+    
+  }// class ends here
   
 
 // to generate number of balls between 1-25
